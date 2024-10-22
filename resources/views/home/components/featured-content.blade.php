14f161a60dbab98234c1375e57ab52e1cd05b427
jcrocker-cmd <narbajajc@gmail.com>
2024-10-22 20:37:21 +0800

    Updated the Homepage

--- resources/views/home/components/featured-content.blade.php
+++ resources/views/home/components/featured-content.blade.php
@@ -7,118 +7,51 @@
         </div>
 
 
-        
         <div class="featured-row-1">
             <div class="featured-col-1">
-                <img src="/images/samplecar2.webp" alt="" srcset="">
+                <img src="/images/mirage.png" alt="Mirage G4">
             </div>
             <div class="featured-col-2">
                 <div class="car-title-wrapper">
-                    <h2 class="car-title">Pick-Up</h2>
+                    <h2 class="car-title">Mitsubishi Mirage G4</h2>
                     <div class="line"></div>
                 </div>
-                <p class="desc">Light Truck</p>
-                <div class="prices">
-                    <h4><sub>₱ </sub>3,500 | Daily</h4>
-                    <h4><sub>₱ </sub>22,000 | Weekly</h4>
-                    <h4><sub>₱ </sub>54,000 | Monthly</h4>
-                </div>
-                <div class="button">
-                    <a href="/pickup">View Rental</a>
-                </div>
-            </div>
-        </div>
-
-
-        <div class="featured-row-2">
-            <div class="featured-col-1">
-                <div class="car-title-wrapper">
-                    <div class="line"></div>
-                    <h2 class="car-title">Van</h2>
-                </div>
-                <p class="desc">Manual</p>
-                <div class="prices">
-                    <h4><sub>₱ </sub>3,800 | Daily</h4>
-                    <h4><sub>₱ </sub>24,000 | Weekly</h4>
-                    <h4><sub>₱ </sub>60,000 | Monthly</h4>
-                </div>
-                <div class="button">
-                    <a href="/van">View Rental</a>
-                </div>
-               
-            </div>
-            <div class="featured-col-2">
-
-                <img src="/images/van.webp" alt="" srcset="">
-            </div>
-        </div>
-
-        <div class="featured-row-1">
-            <div class="featured-col-1">
-                <img src="/images/7.webp" alt="" srcset="">
-            </div>
-            <div class="featured-col-2">
-                <div class="car-title-wrapper">
-                    <h2 class="car-title">7 Seater</h2>
-                    <div class="line"></div>
-                </div>
-                <p class="desc">Innova, Xpander, Suzuki XL-7, Avanza, Ertiga</p>
-                <div class="prices">
-                    <h4><sub>₱ </sub>2,800 | Daily</h4>
-                    <h4><sub>₱ </sub>18,000 | Weekly</h4>
-                    <h4><sub>₱ </sub>45,300 | Monthly</h4>
-                </div>
-                <div class="button">
-                    <a href="/7seaters">View Rental</a>
-                </div>
-            </div>
-        </div>
-
-
-        <div class="featured-row-2">
-            <div class="featured-col-1">
-                <div class="car-title-wrapper">
-                    <div class="line"></div>
-                    <h2 class="car-title">Sedan</h2>
-                </div>
-                <p class="desc">Vios, Ciaz, Mirage G4</p>
+                <p class="desc">Sedan</p>
                 <div class="prices">
                     <h4><sub>₱ </sub>1,800 | Daily</h4>
-                    <h4><sub>₱ </sub>12,600 | Weekly</h4>
-                    <h4><sub>₱ </sub>30,000 | Monthly</h4>
+                    <h4><sub>₱ </sub>11,200 | Weekly</h4>
+                    <h4><sub>₱ </sub>32,000 | Monthly</h4>
                 </div>
                 <div class="button">
-                    <a href="/sedan">View Rental</a>
+                    <a href="/home">View Rental</a>
+                </div>
+            </div>
+        </div>
+
+
+        <div class="featured-row-2">
+            <div class="featured-col-1">
+                <div class="car-title-wrapper">
+                    <div class="line"></div>
+                    <h2 class="car-title">Toyota Vios</h2>
+                </div>
+                <p class="desc">Sedan</p>
+                <div class="prices">
+                    <h4><sub>₱ </sub>1,800 | Daily</h4>
+                    <h4><sub>₱ </sub>11,200 | Weekly</h4>
+                    <h4><sub>₱ </sub>32,000 | Monthly</h4>
+                </div>
+                <div class="button">
+                    <a href="/home">View Rental</a>
                 </div>
                
             </div>
             <div class="featured-col-2">
 
-                <img src="/images/sedan.webp" alt="" srcset="">
+                <img src="/images/sedan.webp" alt="Vios">
             </div>
         </div>
 
         
-        <div class="featured-row-1">
-            <div class="featured-col-1">
-                <img src="/images/wigo.webp" alt="" srcset="">
-            </div>
-            <div class="featured-col-2">
-                <div class="car-title-wrapper">
-                    <h2 class="car-title">Hatchback</h2>
-                    <div class="line"></div>
-                </div>
-                <p class="desc">Mirage GLS, Wigo</p>
-                <div class="prices">
-                    <h4><sub>₱ </sub>1,600 | Daily</h4>
-                    <h4><sub>₱ </sub>9,800 | Weekly</h4>
-                    <h4><sub>₱ </sub>25,000 | Monthly</h4>
-                </div>
-                <div class="button">
-                    <a href="/hatchback">View Rental</a>
-                </div>
-            </div>
-        </div>
-
     </div>
 </section>
